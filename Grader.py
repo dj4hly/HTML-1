7b0be62bb70e69d1e472548238aa63e955e5b59d
dj4hly <sahaidjahly@gmail.com>
2026-09-24 10:56:25 +0200

    side bytte

---
+++ Grader.py
@@ -0,0 +1,7 @@
+x = 5
+y = 10
+
+if x < y:
+    print("x er mindre enn y")
+else:
+    print("x er større eller lik y")
---
+++ Poengsum.py
@@ -0,0 +1,5 @@
+Poengsum = int (input ("Skriv inn poengsummen din: "))
+if Poengsum >= 50:
+    print ("Du har bestått!")
+else:
+    print ("Du har ikke bestått.")
