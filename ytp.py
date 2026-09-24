de9ade7c019eb0ba7af533587d59bbe0873371d2
dj4hly <sahaidjahly@gmail.com>
2026-09-24 09:04:10 +0200

    background change

---
+++ ytp.py
@@ -0,0 +1,13 @@
+a = 1
+b = 15
+x = 0
+
+## hvis verdien til a er større enn 12 så print("HELLO WORLD!")
+
+for x in range(b):
+    if a > 10 and a <=12:
+        print("HELLO WORLD!")
+    x+=1
+    print(x , "Verdien til x")
+    a+=x
+    print(a , "Verdien til a")
